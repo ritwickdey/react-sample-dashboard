ca64ff7c5fa4c33e37264ddf902e240b0e195b9e
Ritwick Dey <ritwickdey@outlook.com>
2018-03-29 20:31:53 +0530

    Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import CSSModules from 'react-css-modules';
 
-const Portfolio = props => <div>Portfolio</div>;
+import styles from './Portfolio.module.css';
 
-export default Portfolio
+const Portfolio = props => (
+  <div>
+    <div styleName="header">
+      <div>
+        TYPE
+        <i className="material-icons" styleName="icon">
+          keyboard_arrow_down
+        </i>
+      </div>
+      <div>
+        DATE
+        <i className="material-icons" styleName="icon">
+          keyboard_arrow_down
+        </i>
+      </div>
+      <div>
+        VALUE
+        <i className="material-icons" styleName="icon">
+          keyboard_arrow_down
+        </i>
+      </div>
+    </div>
+  </div>
+);
+
+export default CSSModules(Portfolio, styles);
